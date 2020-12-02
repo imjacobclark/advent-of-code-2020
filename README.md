1761797700c4af30b3096024e572ef52a70ff440
Jacob Clark <jacob.jh.clark@googlemail.com>
2020-12-02 22:43:54 +0000

    Day 2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # advent-of-code-2020
 Advent of Code 2020 Solutions
+
+__Language__: Rust
+
+None of the code in this repository is production quality, I don't write Rust in my day job, this is just a bit of fun over Christmas!
+
+Merry Christmas 2020 - Jake
--- src/two.rs
+++ src/two.rs
@@ -4,30 +4,41 @@ pub fn get_occurrences(sequence: Vec<String>, requirement: String) -> usize {
   sequence.iter().filter(|&a| a.to_string() == requirement).count()
 }
 
+pub fn get_char_at_index(sequence: Vec<String>, index: usize) -> String {
+  return sequence.into_iter().nth(index - 1).unwrap();
+}
+
 pub fn password_policy() {
   let passwords = fs::read_to_string("./input/two.txt")
-    .expect("Something went wrong reading the file");
+  .expect("Something went wrong reading the file");
 
-  let passwords: Vec<bool> = passwords
-    .lines()
-    .map(|password| {
-      let mut strings = password.split_whitespace();
-      let rules = strings.next().unwrap().to_string();
+let passwords: Vec<(bool, bool)> = passwords
+  .lines()
+  .map(|password| {
+    let mut strings = password.split_whitespace();
+    let rules = strings.next().unwrap().to_string();
 
-      let mut rules_iter = rules.split("-").map(|s| s.to_string());
-      let min = rules_iter.next().unwrap();
-      let max = rules_iter.next().unwrap();
+    let mut rules_iter = rules.split("-").map(|s| s.to_string());
+    let min = rules_iter.next().unwrap();
+    let max = rules_iter.next().unwrap();
 
-      let requirement = strings.next().unwrap().to_string().replace(":", "");
-      let password: Vec<String> = strings.next().unwrap().to_string().split("").map(|s| s.to_string()).collect();
-      
-      let occurrences = get_occurrences(password.clone(), requirement.clone());
-      let has_min = occurrences >= min.parse().unwrap();
-      let has_max = occurrences <= max.parse().unwrap();
+    let requirement = strings.next().unwrap().to_string().replace(":", "");
+    let mut password: Vec<String> = strings.next().unwrap().to_string().split("").map(|s| s.to_string()).collect::<Vec<String>>();
+    password.remove(0);
+    password.remove(password.len()-1);
 
-      has_min && has_max
-    })
-    .collect();
+    // Part 1
+    let occurrences = get_occurrences(password.clone(), requirement.clone());
+    let has_min = occurrences >= min.parse().unwrap();
+    let has_max = occurrences <= max.parse().unwrap();
 
-  println!("Day 2, Task 1, Method 1: {:?}", passwords.iter().filter(|p| **p == true).count());
+    // Part 2
+    let ok = (get_char_at_index(password.clone(), min.parse().unwrap()) == requirement) ^ (get_char_at_index(password.clone(), max.parse().unwrap()) == requirement);
+
+    (has_min && has_max, ok)
+  })
+  .collect();
+
+  println!("Day 2, Task 1, Method 1: {:?}", passwords.iter().filter(|p| p.0 == true).count());
+  println!("Day 2, Task 2, Method 1: {:?}", passwords.iter().filter(|p| p.1 == true).count());
 }
